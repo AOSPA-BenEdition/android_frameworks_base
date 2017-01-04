d1974043de96b087d8ef166004df1f0fb4dd8ca0
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    [Squashed] Status network traffic [1/2]
    Status network traffic [1/2]
    
    -----
    abc ezio84:
    use O darkintensity api
    use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
    fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
    don't show arrows when no data
    remove unit settings (KB is good)
    remove interval (set to 1sec by def) and single arrow options
    don't get settings values again when not needed
    fix padding between arrows and speed values and expose the padding value
    code cleanup
    -----
    
    Fix janky traffic indicator tint
    
    This is best explained by looking at the jank
    
    https://i.imgur.com/EajdLei.png
    
    Change-Id: I8d4df1aab769755a939dcba6571cb5f0e09b243d
    
    NetworkTraffic: Fix gravity
    
    TextView.setText() has left gravity by default. It's ugly in statusbar
    
    Statusbar net monitor: stop the handler if screen is off
    
    no need to keep the net monitor working if screen is off
    and waste system / battery resources
    
    Change-Id: I977eee3b38d19a755b9e9a0b38c13e86aea433c5
    
    Network traffic: skip more useless handler calls
    
    Add network traffic monitor properly to statusbar icons
    
    Make it behave fine on devices that don't have much status bar space
    (e.g. because of a display cutout/"notch")
    
    Network traffic: fix static color usage
    
    After setting a static color, we don't want to react to onDarkChanged
    anymore.
    This fixes wrong color in quick settings when qs have different color
    scheme than status bar.
    
    NetworkTraffic: fix indicator not hiding on lost connection
    
    With https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/80636
    ("Statusbar net monitor: stop the handler if screen is off"),
    connectivity status can change without us noticing, so let's update in
    updateSettings() (which is called after turning the screen on) also if
    there is no connection.
    
    Change-Id: I674f9cc3946f679b1ff9696079c5608fdfaa12d7
    
    NetworkTraffic: Replace arrows drawable with unicode symbols
    
    NetworkTraffic: Fix speed unit
    
    We have B, KB, MB per second
    
    NetworkTraffic: Update for Android 10

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4890,6 +4890,24 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+        /** @hide */
+        private static final Validator NETWORK_TRAFFIC_STATE_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+         /** @hide */
+        private static final Validator NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4962,6 +4980,8 @@ public final class Settings {
             ENABLE_SUGGESTIONS,
             QS_PANEL_BG_ALPHA,
             SCREENSHOT_SHUTTER_SOUND,
+            NETWORK_TRAFFIC_STATE,
+            NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         };
 
         /**
@@ -5114,6 +5134,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ENABLE_SUGGESTIONS);
             PRIVATE_SETTINGS.add(QS_PANEL_BG_ALPHA);
             PRIVATE_SETTINGS.add(SCREENSHOT_SHUTTER_SOUND);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
 
         /**
@@ -5221,6 +5243,8 @@ public final class Settings {
             VALIDATORS.put(ENABLE_SUGGESTIONS, ENABLE_SUGGESTIONS_VALIDATOR);
             VALIDATORS.put(QS_PANEL_BG_ALPHA, QS_PANEL_BG_ALPHA_VALIDATOR);
             VALIDATORS.put(SCREENSHOT_SHUTTER_SOUND, SCREENSHOT_SHUTTER_SOUND_VALIDATOR);
+            VALIDATORS.put(NETWORK_TRAFFIC_STATE, NETWORK_TRAFFIC_STATE_VALIDATOR);
+            VALIDATORS.put(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD_VALIDATOR);
         }
 
         /**
